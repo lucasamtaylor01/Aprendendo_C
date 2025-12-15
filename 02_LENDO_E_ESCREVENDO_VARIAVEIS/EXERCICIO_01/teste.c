4afd0f38e1eabaace5c27447b322d5ad074abb2f
lucasamtaylor01 <zocyus@protonmail.com>
2025-12-15 03:46:20 -0300

    Primeiro passo

---
+++ 02_LENDO_E_ESCREVENDO_VARIAVEIS/EXERCICIO_01/teste.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int x, sucessor, antecessor;
+    printf("Insira um número: ");
+    scanf("%d", &x);
+
+    sucessor = x+1;
+    antecessor = x-1;
+    printf("O sucessor é: %d\n", sucessor);
+    printf("O antecessor é: %d\n", antecessor);
+
+    return 0;
+}
